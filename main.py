85dd9b59414a2bf9e1da81541e9569a1f6f0eee2
AliensCanCode <>
2021-04-12 05:13:08 +0000

    Deleted active channel check

--- main.py
+++ main.py
@@ -19,14 +19,15 @@ async def ping(ctx):
     # This tells us the bot's ping.
     await ctx.send(f'Pong! {client.latency}')
 
-@tasks.loop(seconds=60)
+@tasks.loop(seconds=12)
 async def meow_task():
-    if random.randint(1,10) == 1:
+    if random.randint(1,32) == 1:
         # Gets Greyson's server
         greyson_groop = client.get_guild(828581824711753738)
         # Gets the proper channel
         gg_channel = greyson_groop.get_channel(828581824711753741)
         # Sends the message!
+        # We need to check if the channel is active or not.
         await gg_channel.send(random.choice(['meow','meow!','Meow!','Meow','*Meow!*','meaw','Meaw!']))
 
 meow_task.start()
